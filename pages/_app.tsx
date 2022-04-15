29d0519b02b1028118c8bcdf86a394008836fa01
manzur1990 <manzur-1990@hotmail.com>
2022-04-14 21:54:59 -0500

    added chakraPriveder to wrap around Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,11 @@ const theme = extendTheme({
 });
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
 export default MyApp;
