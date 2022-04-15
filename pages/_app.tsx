6022f9fd1b40376f19197481e6192c0f80da025b
manzur1990 <manzur-1990@hotmail.com>
2022-04-14 21:52:13 -0500

    chakra setup with extendTheme: button variant/link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import 'reset-css';
+
+const theme = extendTheme({
+    colors: {
+        gray: {
+            100: '#F5F5F5',
+            300: '#EEEEEE',
+            400: '#BDBDBD',
+            500: '#757575',
+            600: '#616161',
+            800: '#424242',
+            900: '#212121',
+        },
+    },
+    components: {
+        Buton: {
+            variants: {
+                link: {
+                    ':focus': {
+                        outline: 'none',
+                        boxShadow: 'none',
+                    },
+                },
+            },
+        },
+    },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
