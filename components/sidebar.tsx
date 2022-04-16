eeb1046e033733dee7a61f6fcb9b0f1b309b571e
manzur1990 <manzur-1990@hotmail.com>
2022-04-16 18:00:44 -0500

    revamped nav-items into seperate components

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,30 +1,8 @@
 import NextImage from 'next/image';
-import NextLink from 'next/link';
+import NavigationItems from '../components/navigationItems';
+import MusicMenu from '../components/musicMenu';
 
-import {
-    Box,
-    List,
-    ListItem,
-    ListIcon,
-    Divider,
-    Center,
-    LinkBox,
-    LinkOverlay,
-} from '@chakra-ui/layout';
-
-import {
-    MdHome,
-    MdSearch,
-    MdLibraryMusic,
-    MdPlayListAdd,
-    MdFavorite,
-} from 'react-icons/md';
-
-const navMenu = [
-    { name: 'Home', icon: MdHome, route: '/' },
-    { name: 'Search', icon: MdSearch, route: '/search' },
-    { name: 'Your Library', icon: MdLibraryMusic, route: '/library' },
-];
+import { Box, Divider, Center } from '@chakra-ui/layout';
 
 const Sidebar = () => {
     return (
@@ -38,29 +16,9 @@ const Sidebar = () => {
                 <Box width='120px' marginBottom='20px' paddingX='20px'>
                     <NextImage src='/logo.png' height={100} width={100} />
                 </Box>
-                <Box marginBottom='20px'>
-                    <List spacing={2}>
-                        {navMenu.map((menu) => (
-                            <ListItem
-                                paddingX='20px'
-                                fontSize='16px'
-                                key={menu.name}>
-                                <LinkBox>
-                                    <NextLink href={menu.route} passHref>
-                                        <LinkOverlay>
-                                            <ListIcon
-                                                as={menu.icon}
-                                                color='white'
-                                                marginRight='20px'
-                                            />
-                                            {menu.name}
-                                        </LinkOverlay>
-                                    </NextLink>
-                                </LinkBox>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
+                <NavigationItems />
+                <Divider color='gray.900' />
+                <MusicMenu />
             </Box>
         </Box>
     );
