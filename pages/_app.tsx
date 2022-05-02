27151c1fae55035be2391a4dde41341ba764a25f
manzur1990 <manzur-1990@hotmail.com>
2022-05-02 12:14:23 -0600

    added signin and signup functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,45 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import 'reset-css';
-import PlayerLayout from '../components/playerLayout';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import 'reset-css'
+import PlayerLayout from '../components/playerLayout'
 
 const theme = extendTheme({
-    colors: {
-        gray: {
-            100: '#F5F5F5',
-            300: '#EEEEEE',
-            400: '#BDBDBD',
-            500: '#757575',
-            600: '#616161',
-            800: '#424242',
-            900: '#212121',
-        },
+  colors: {
+    gray: {
+      100: '#F5F5F5',
+      300: '#EEEEEE',
+      400: '#BDBDBD',
+      500: '#757575',
+      600: '#616161',
+      800: '#424242',
+      900: '#212121',
     },
-    components: {
-        Button: {
-            variants: {
-                link: {
-                    ':focus': {
-                        outline: 'none',
-                        boxShadow: 'none',
-                    },
-                },
-            },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
         },
+      },
     },
-});
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <ChakraProvider theme={theme}>
-            <PlayerLayout>
-                <Component {...pageProps} />
-            </PlayerLayout>
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider theme={theme}>
+      {Component.authPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <PlayerLayout>
+          <Component {...pageProps} />
+        </PlayerLayout>
+      )}
+    </ChakraProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
