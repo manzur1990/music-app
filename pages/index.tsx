33d47e69565d5072a9a9fbe78a513d147da6e7aa
manzur1990 <manzur-1990@hotmail.com>
2022-05-09 16:03:44 -0600

    fixed layout design on playet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return <div>home</div>;
+  return (
+    <GradientLayout
+      color="pink"
+      subtitle="profile"
+      title="Jorge Manzur"
+      description="15 public playlists"
+      image={
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Banded_Mongoose_Nose_Detail%2C_crop.jpg/800px-Banded_Mongoose_Nose_Detail%2C_crop.jpg'
+      }
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  )
 }
