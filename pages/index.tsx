8e7a8effefcc4a3985a6a1e83a779c2f4182482f
manzur1990 <manzur-1990@hotmail.com>
2022-05-11 12:14:57 -0600

    added dynamic data to user's name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import GradientLayout from '../components/gradientLayout'
+import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
 import styles from '../styles/Home.module.css'
 
 const Home = ({ artists }) => {
+  const { user, isLoading } = useMe()
   return (
     <GradientLayout
-      color="pink"
+      color="gray"
       subtitle="profile"
-      title="Jorge Manzur"
+      title={`${user?.firstName} ${user?.lastName}`}
       description="15 public playlists"
       image={
         'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Banded_Mongoose_Nose_Detail%2C_crop.jpg/800px-Banded_Mongoose_Nose_Detail%2C_crop.jpg'
@@ -22,7 +24,7 @@ const Home = ({ artists }) => {
 
 export const getServerSideProps = async () => {
   const artists = await prisma.artist.findMany({})
-  console.log(artists)
+
   return {
     props: { artists },
   }
