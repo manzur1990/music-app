6aacca794b678e00c3ce59b0e8cf967a6a8a03f7
manzur1990 <manzur-1990@hotmail.com>
2022-05-11 12:14:25 -0600

    added req parameter to user's api call

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,7 +1,7 @@
 import { validateRoute } from '../../lib/auth'
 import prisma from '../../lib/prisma'
 
-export default validateRoute(async (res, user) => {
+export default validateRoute(async (req, res, user) => {
   const playlistsCount = await prisma.playlist.count({
     where: {
       userId: user.id,
