64d811b3c2b49e8e4db8272bd159fbe87fdacc76
manzur1990 <manzur-1990@hotmail.com>
2022-05-14 08:44:21 -0600

    added user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,48 @@
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import Head from 'next/head'
-import Image from 'next/image'
+
 import GradientLayout from '../components/gradientLayout'
 import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
 import styles from '../styles/Home.module.css'
 
 const Home = ({ artists }) => {
-  const { user, isLoading } = useMe()
+  const { user } = useMe()
   return (
     <GradientLayout
       color="gray"
       subtitle="profile"
       title={`${user?.firstName} ${user?.lastName}`}
-      description="15 public playlists"
+      description={`${user?.playlistsCount} public playlists`}
       image={
         'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Banded_Mongoose_Nose_Detail%2C_crop.jpg/800px-Banded_Mongoose_Nose_Detail%2C_crop.jpg'
       }
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
 }
