8dd084d75b4c6e2c26903a0a3764a771a716ab8c
manzur1990 <manzur-1990@hotmail.com>
2022-05-16 10:49:40 -0600

    added colors to playlist page

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,27 @@
+import GradientLayout from '../../components/gradientLayout'
 import { validateToken } from '../../lib/auth'
 import prisma from '../../lib/prisma'
 
-const Playlist = () => {
-  return <div>Playlist</div>
+const getRGBColor = (id) => {
+  const colors = [
+    'red',
+    'greed',
+    'blue',
+    'orange',
+    'purple',
+    'gray',
+    'teal',
+    'yellow',
+  ]
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
+const Playlist = ({ playlist }) => {
+  const color = getRGBColor(playlist.id)
+  return (
+    <GradientLayout color={color}>
+      <div>dfd</div>
+    </GradientLayout>
+  )
 }
 
 export const getServerSideProps = async ({ query, req }) => {
