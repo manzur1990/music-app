8a9b9d2d2d8450579e439ef740deb6358085439e
manzur1990 <manzur-1990@hotmail.com>
2022-06-06 11:56:26 -0600

    changed Box property to overflowY

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -13,7 +13,7 @@ const GradientLayout = ({
   return (
     <Box
       height="100%"
-      overflow="auto"
+      overflowY="auto"
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0.95) 75%)`}
     >
       <Flex bg={`${color}.600`} padding="40px" align="end">
