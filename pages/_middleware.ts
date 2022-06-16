a16d5c6cf5fc3eb753683d74cbe9f7fdd3c8bcea
manzur1990 <manzur-1990@hotmail.com>
2022-06-15 19:18:23 -0600

    fixed middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,15 +1,13 @@
-// pages/_middleware.ts
-const signinInPages = ['/', '/playlist', 'library']
-
-import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  if (signinInPages.find((p) => p === request.nextUrl.pathname)) {
-    const token = request.cookies.TRAX_ACCESS_TOKEN
+const signedinPages = ['/', '/playlist', '/library']
+
+export default function middleware(req) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN
 
     if (!token) {
-      return NextResponse.redirect(new URL('/signin', request.url))
+      return NextResponse.redirect('/signin')
     }
   }
 }
